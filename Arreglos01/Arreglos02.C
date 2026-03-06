b35176e2ca81a0d19fc7db275fbeac4c38502746
renataastorga2404-pixel <renata.astorga2404@alumnos.udg.mx>
2026-03-06 08:41:44 -0600

    Update Arreglos02.C

--- Arreglos01/Arreglos02.C
+++ Arreglos01/Arreglos02.C
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+int main()
+{
+  int N=4;//Define el tamaño de mi arreglo
+  float temp[N];
+
+  for(int h=0; h < N; h++)
+  {
+      printf("Ingresa la temperatura:\n");
+      scanf("%f",&temp[h]);
+  }
+
+  for(int i=0; i< N ; i++)
+  {
+    printf("%.2f\n", temp[i]);
+
+  }
+
+  return 0;
+}
