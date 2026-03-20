0dd466f08c9c8711bb07c9d257c1b8535a6d7f92
renataastorga2404-pixel <renata.astorga2404@alumnos.udg.mx>
2026-03-20 08:44:14 -0600

    Create arreglo.c to demonstrate array usage in C
    Added a C program that initializes an array and prints its values.

---
+++ 20demarzo/arreglo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+//char numero[5];
+
+ //for(int i=0; i<5; i++){
+    //printf("%c\n", &numero[i]);}
+    //printf("%c", numero);
+
+int num[5];
+for(int i=0; i<5; i++){
+    printf("%d\n",&num[i]);
+}
+ for(int j=0; j<5; j++){
+    printf("%d\n", num[j]);
+ }
+return 0;}
