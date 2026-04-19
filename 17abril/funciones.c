1cdb31d7d962a50229ecc58bb53c5d3e9c312111
renataastorga2404-pixel <renata.astorga2404@alumnos.udg.mx>
2026-04-19 14:22:59 -0600

    Implement file output for original and filtered arrays
    Added a function to write original and filtered arrays to a file.

--- 17abril/funciones.c
+++ 17abril/funciones.c
@@ -21,23 +21,40 @@ for(int i=0; i<n; i++){
             }
         }
  }
+void archivo(int arreglo1[], int n1,int arreglo2[], int n2){
+ FILE *f;
+ f=fopen ("resultado.txt","w");
+
+ fprintf(f,"Arreglo original:\n");
+ for(int i=0; i<n1; i++){
+    fprintf(f,"%d", arreglo1[i]);
+
+ }
+ fprintf(f,"\n");
+
+ fprintf(f,"Arreglo filtrado:\n");
+ for(int j=0; j<n2; j++){
+    fprintf(f,"%d",arreglo2[j]);
+ }
+ fprintf(f,"\n");
+ fclose(f);
+ }
 
 
 int main()
 {
     int n=6;
-    int arre[n];
+    int arreglo1[6];
 
-    int arreglo1[6]={1,2,3,4,5,6};
-    mostrar(arreglo1,6);
     int arreglo2[3];
 
-    capturar(arre,n);
-    mostrar(arre,n);
-
+    capturar(arreglo1,n);
+    mostrar(arreglo1,n);
 
  filtrar(arreglo1,arreglo2,n);
 
   mostrar(arreglo2,3);
+
+  archivo(arreglo1,n,arreglo2,3);
     return 0;
 }
